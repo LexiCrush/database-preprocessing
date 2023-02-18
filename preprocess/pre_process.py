4f749830f52541b251d3d8cfb539550cd9374a18
nicolettethiro <nicolette.thiro@cooper.edu>
2023-02-18 18:22:56 -0500

    body

--- preprocess/pre_process.py
+++ preprocess/pre_process.py
@@ -47,7 +47,7 @@ roman_gods = preprocess_one_col_csv('./nounbanks/ROMAN_GODS.csv')
 cosmetic_items = preprocess_one_col_csv('./nounbanks/COSMETIC_ITEMS.csv')
 office_supplies = preprocess_one_col_csv('./nounbanks/OFFICE_SUPPLIES.csv')
 disney_movies = preprocess_one_col_csv('./nounbanks/DISNEY_MOVIES.csv')
-
+body_parts = preprocess_one_col_csv('./nounbanks/BODY_PARTS.csv')
 
 
 
@@ -68,3 +68,5 @@ print("\n\n\n")
 print(office_supplies.head(20))
 print("\n\n\n")
 print(disney_movies.head(20))
+print("\n\n\n")
+print(body_parts.head(20))
